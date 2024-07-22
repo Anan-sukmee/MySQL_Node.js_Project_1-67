489cb3dff26cfd5a57a7e4986568104e104fe882
Anan-sukmee <anan.su611@gmail.com>
2024-07-22 19:28:34 +0700

    fix commit

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const PORT = 3000
 app.listen(PORT,()=>{
     console.log('API Listening on PORT ${PORT}')
 })
-app.get("/item", async (req, res) =>{
-    res.json(results)
+app.get("/item", async (req, res) => {
+    try {
+        connection.query("SELECT * FROM item", (err, results, fields) => {
+            if (err) {
+                console.log(err);
+                return res.status(400).send();
+            }
+            res.status(200).json(results)
+        })
+    } catch(err) {
+        console.log(err);
+        return res.status(500).send();
+    }
 })
 module.exports =app
